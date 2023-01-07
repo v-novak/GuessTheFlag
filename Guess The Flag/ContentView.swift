f9dc7317490449e689bd5724bdb4b693a5f14224
Vasiliy Novak (Sabadazh) <vnovak2048@gmail.com>
2023-01-08 02:01:08 +0400

    initial commit

---
+++ Guess The Flag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Guess The Flag
+//
+
+import SwiftUI
+
+struct ContentView: View
+{
+    var body: some View {
+        ZStack {
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,15 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
